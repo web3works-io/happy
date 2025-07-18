e891b9f81ccde41d83a6ccff7bf20f37fad69cb9
Steve Korshakov <steve@korshakov.com>
2025-07-18 09:03:17 -0700

    fix: remove logging

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,5 @@
 module.exports = function (api) {
   api.cache(true);
-  console.error("trying to resolve nativewind")
-  console.log(__dirname)
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
   };
