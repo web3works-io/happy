6ccb15b8e2a4ae2220388313c9ba5e00e2875f7d
Steve Korshakov <steve@korshakov.com>
2025-07-31 09:45:42 -0700

    fix: fix missing android link

--- release-native.sh
+++ release-native.sh
@@ -4,3 +4,4 @@ eas build --profile development-store --platform ios --auto-submit-with-profile=
 eas build --profile preview --platform ios --no-wait --non-interactive
 eas build --profile preview-store --platform ios --auto-submit-with-profile=production --no-wait --non-interactive
 eas build --profile production --platform ios --auto-submit-with-profile=production --no-wait --non-interactive
+eas build --profile production --platform android --auto-submit-with-profile=production --no-wait --non-interactive
