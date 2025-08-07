1a9d3ecd81941232f60844b59dfeb4147f4060bb
Steve Korshakov <steve@korshakov.com>
2025-08-07 13:44:55 -0700

    ref: preview -> dev

--- release-preview.sh
+++
@@ -1,3 +0,0 @@
-set -e
-eas build --profile preview --platform ios --no-wait --non-interactive
-eas build --profile preview-store --platform ios --auto-submit-with-profile=production --no-wait --non-interactive
