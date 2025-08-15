cfe9846e8956446c87ba94fe06a2e01fe33a0d5a
Steve Korshakov <steve@korshakov.com>
2025-08-15 14:25:58 -0700

    ref: revert skia loading

--- _index.web.ts
+++
@@ -1,9 +0,0 @@
-import '@expo/metro-runtime';
-import { App } from 'expo-router/build/qualified-entry';
-import { renderRootComponent } from 'expo-router/build/renderRootComponent';
-
-import { LoadSkiaWeb } from '@shopify/react-native-skia/lib/module/web';
-
-LoadSkiaWeb().then(async () => {
-    renderRootComponent(App);
-});
--- index.ts
+++
@@ -1,3 +0,0 @@
-import { renderRootComponent } from 'expo-router/build/renderRootComponent';
-import { App } from 'expo-router/build/qualified-entry';
-renderRootComponent(App);
